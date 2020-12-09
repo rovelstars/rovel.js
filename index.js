099a6f6b1bff5b90184198f1533b687af145d36e
sayantan300 <rovelstars300@gmail.com>
2020-12-09 13:58:13 +0530

    hmm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
-const { exec } = require('child_process');
-const baseurl = "https://rovelapi.glitch.me";
+const fetch = require('node-fetch')
+const { exec } = require('child_process')
+const baseurl = "https://rovelapi.glitch.me"
 
 function guildstats(botid, key, guild) {
 	fetch(`https://bots.rovelstars.ga/api/v1/bots/${botid}/stats`, {
@@ -10,14 +10,14 @@ function guildstats(botid, key, guild) {
 			'Content-Type': 'application/json'
 		},
 		method: 'POST',
-	});
-};
-async function chat(userid, msg) {
-	return await fetch(baseurl+`/chat?user=${userid}&msg=${msg}`).then(r=>r.text())};
-async function base() {
-	return await fetch(baseurl).then(res => res.text())
-	};
-function rovelexec(msg) {
-	exec(msg);
+	})
 }
-module.exports = {guildstats, chat, base, rovelexec};
+async function chat(userid, msg) {
+	return await fetch(baseurl+`/chat?user=${userid}&msg=${msg}`).then(r=> {r.text()})}
+async function base() {
+	return await fetch(baseurl).then(res => { res.text() })
+	}
+function rovelexec(msg) {
+	exec(msg)
+}
+module.exports = {guildstats, chat, base, rovelexec}
